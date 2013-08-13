b01bf9564714946d67baa0f393828e1e7e564414
kotoroto <mates@sharklasers.com>
2013-08-13 16:13:58 +0200

    small update to main file

--- yjd_cli.py
+++ yjd_cli.py
@@ -11,13 +11,17 @@ import getopt
 import random
 from colorama import Fore, init, Style ## Deze installeren uit repo's
 from time import gmtime, strftime
+from datetime import datetime
+
 
 def downloaderAansturen():
     dl = Downloader()
     dl.titelAanmaken()
     dl.openEmbedpagina()
     dl.vind_oorspronkelijke_video()
+    dl.starttijdNoteren()
     dl.oorspronkelijke_video_downloaden()
+    dl.eindtijdNoteren()
 
 class Downloader():
     def __init__(self):
@@ -43,19 +47,19 @@ class Downloader():
         
         for titeloptie in titelopties:
             if titeloptie[0] == "-n":
-                print "Will use normal title with dashes instead of spaces."
+                print "Will use normal title with dashes instead of spaces:",
                 self.opslaantitel = self.paginatitel + ".flv"
                 
             elif titeloptie[0] == "-r":
-                print "Will use random number"
+                print "Will use random number:",
                 self.opslaantitel = str(random.randint(0,9999)) + ".flv"
                 
             elif titeloptie[0] == "-i":
-                print "Zal video-ID gebruiken"
+                print "Will use video ID:",
                 self.opslaantitel = self.videoid + ".flv"
             
             elif titeloptie[0] == "-a":
-                print "Will use abbreviated title"
+                print "Will use abbreviated title:",
                 self.paginatitel_met_spaties = self.paginatitel.replace("-", " ")
                 
                 self.paginatitel_eerste_letters = ""
@@ -66,7 +70,7 @@ class Downloader():
                 self.opslaantitel = self.paginatitel_eerste_letters + ".flv"
             
             else:
-                print "Will use normal title with dashes instead of spaces."
+                print "Will use normal title with dashes instead of spaces:",
                 self.opslaantitel = self.paginatitel + ".flv"
                 
                 
@@ -104,36 +108,62 @@ class Downloader():
         
         #print self.oorspronkelijke_video_url
         
-
+    def starttijdNoteren(self):
+        self.t1 = datetime.now()
 
     def oorspronkelijke_video_downloaden(self):
         u = urllib2.urlopen(self.oorspronkelijke_video_url)
-        file_name = self.opslaantitel
-        f = open(file_name, 'wb')
+        self.file_name = self.opslaantitel
+        f = open(self.file_name, 'wb')
         meta = u.info()
-        file_size = int(meta.getheaders("Content-Length")[0])
+        self.file_size = int(meta.getheaders("Content-Length")[0])
+        self.file_size_in_mb = int(self.file_size/1048576)
+        
+        self.file_size_in_mb_string_length = len(str(self.file_size_in_mb))
+        sys.stdout.write("File size....." + Style.BRIGHT + Fore.RESET + "[" + Fore.GREEN + str(self.file_size_in_mb) + " " * (4-self.file_size_in_mb_string_length) + "MB" + Fore.RESET + "]" + Style.RESET_ALL + "\n")
+        sys.stdout.flush()
 
-        file_size_dl = 0
-        block_sz = 8192
+        self.file_size_dl = 0
+        self.block_sz = 8192
         while True:
-            buffer = u.read(block_sz)
+            buffer = u.read(self.block_sz)
             if not buffer:
                 break
 
-            file_size_dl += len(buffer)
+            self.file_size_dl += len(buffer)
             f.write(buffer)
-            #status = r"Downloading...[ %2.0f%%  ]" % (file_size_dl * 100. / file_size)
-            status = r"Downloading..." + Style.BRIGHT + Fore.RESET + "[ " + Fore.GREEN + "%2.0f%%" % (file_size_dl * 100. / file_size) + Fore.RESET + "  ]" + Style.RESET_ALL 
+            status = r"Downloading..." + Style.BRIGHT + Fore.RESET + "[" + Fore.GREEN + "%2.0f   %%" % (self.file_size_dl * 100. / self.file_size) + Fore.RESET + "]" + Style.RESET_ALL 
             status = status + chr(8)*(len(status)+1)
             #print status,
             sys.stdout.write("\r" + status,)
-            sys.stdout.flush
+            sys.stdout.flush()
 
         f.close()
-        sys.stdout.write("\r" + r"Downloading..." + Style.BRIGHT + Fore.RESET + "[  " + Fore.GREEN + "OK" + Fore.RESET + "   ]" + Style.RESET_ALL )
-        sys.stdout.write("\n") # Zorgen dat de prompt weer gewoon op de volgende regel start.
-        print "Download completed on",
-        print(strftime("%Hh%M"))
-        print "Size: %d MB" % (int(file_size/1048576)) ## divide by digit to convert bytes to MB.
+        sys.stdout.write("\r")
+        sys.stdout.write(" " * 25)
+        sys.stdout.write("\r")
+        sys.stdout.flush()
+        print("Downloading..." + Style.BRIGHT + Fore.RESET + "[  " + Fore.GREEN + "OK" + Fore.RESET + "  ]" + Style.RESET_ALL)
+        #sys.stdout.write("\n") # Zorgen dat de prompt weer gewoon op de volgende regel start.
+        sys.stdout.flush()
+        
+        
+    def eindtijdNoteren(self):
+        self.t2 = datetime.now()
+        
+        self.delta = (self.t2 - self.t1)
+        self.file_size_in_kb = int(self.file_size/1024) ## needed later
+        #print("aantal seconden = " + str(self.delta.seconds))
+        self.avg_speed = self.file_size_in_kb/(self.delta.seconds)
+        self.avg_speed_string_length = len(str(self.avg_speed))
+        
+        sys.stdout.write("Avg speed....." + Style.BRIGHT + Fore.RESET + "[" + Fore.GREEN + " " * ((6-self.file_size_in_mb_string_length)/2) + str(self.avg_speed) + " " * ((6-self.file_size_in_mb_string_length)/2) + Fore.RESET + "]" + Style.RESET_ALL + "\n")
+        sys.stdout.flush() ## Moet nog aan gewerkt worden! Hij moet bvb 192 netjes in het midden zetten, als dat niet gaat, evt li 1 en re 2 vrijlaten.
+        ## OPLOSSING: gewoon een len() invoegen en twee ifs.
+        
+        print("Completed on.." + Style.BRIGHT + Fore.RESET + "[" + Fore.GREEN + strftime("%H%M%S") + Fore.RESET + "]" + Style.RESET_ALL)
+        
+        #print "Download completed on",
+        #print(strftime("%Hh%M"))
 
 downloaderAansturen()
